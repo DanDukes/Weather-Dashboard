641e8226caab952f36f0d50897864b820cc0f78e
Dan D <degeare28@gmail.com>
2019-12-16 18:12:42 -0700

    roadmap comments

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  //Declare Variables
+  //Event Listener on the Button
+  //Function to Populate Search History List
+  //Event Listener on Search History Buttons
+  //Function to cal Weather API
+  //Function to Parse API Return and build current conditions card
+  //Function to build 5 day forecast cards
+});
