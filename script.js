4a4a9b36ac44fce682ffda5b1e1edfe1644d7c01
Dan D <degeare28@gmail.com>
2020-04-23 15:50:14 -0600

    added clickable UV Button

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
       url:
         "http://api.openweathermap.org/data/2.5/weather?q=" +
         searchValue +
-        "&appid=7ba67ac190f85fdba2e2dc6b9d32e93c&units=imperial",
+        "&appid=adfa30d28992b81767460464c73f76b5&units=imperial",
       dataType: "json",
       success: function(data) {
         // create history link for this search
@@ -41,7 +41,7 @@ $(document).ready(function() {
 
         // create html content for current weather
         var title = $("<h3>")
-          .addClass("card-title")
+          .addClass("card-title bg-info")
           .text(data.name + " (" + new Date().toLocaleDateString() + ")");
         var card = $("<div>").addClass("card");
         var wind = $("<p>")
@@ -53,7 +53,7 @@ $(document).ready(function() {
         var temp = $("<p>")
           .addClass("card-text")
           .text("Temperature: " + data.main.temp + " °F");
-        var cardBody = $("<div>").addClass("card-body");
+        var cardBody = $("<div>").addClass("card-body bg-info");
         var img = $("<img>").attr(
           "src",
           "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
@@ -78,13 +78,13 @@ $(document).ready(function() {
       url:
         "http://api.openweathermap.org/data/2.5/forecast?q=" +
         searchValue +
-        "&appid=7ba67ac190f85fdba2e2dc6b9d32e93c&units=imperial",
+        "&appid=adfa30d28992b81767460464c73f76b5&units=imperial",
       dataType: "json",
       success: function(data) {
         // overwrite any existing content with title and empty row
         $("#forecast")
-          .html('<h4 class="mt-3">5-Day Forecast:</h4>')
-          .append('<div class="row">');
+          .html('<h4 class="mt-3 bg-secondary">5-Day Forecast:</h4>')
+          .append('<div class="row bg-secondary">');
 
         // loop over all forecasts (by 3-hour increments)
         for (var i = 0; i < data.list.length; i++) {
@@ -92,7 +92,7 @@ $(document).ready(function() {
           if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
             // create html elements for a bootstrap card
             var col = $("<div>").addClass("col-md-2");
-            var card = $("<div>").addClass("card bg-primary text-white");
+            var card = $("<div>").addClass("card bg-info text-white");
             var body = $("<div>").addClass("card-body p-2");
 
             var title = $("<h5>")
@@ -126,16 +126,18 @@ $(document).ready(function() {
     $.ajax({
       type: "GET",
       url:
-        "http://api.openweathermap.org/data/2.5/uvi?appid=7ba67ac190f85fdba2e2dc6b9d32e93c&lat=" +
+        "http://api.openweathermap.org/data/2.5/uvi?appid=adfa30d28992b81767460464c73f76b5&lat=" +
         lat +
         "&lon=" +
         lon,
       dataType: "json",
       success: function(data) {
         var uv = $("<p>").text("UV Index: ");
-        var btn = $("<span>")
+        var btn = $("<a>")
           .addClass("btn btn-sm")
-          .text(data.value);
+          .text(data.value)
+          .attr("href", "https://www.epa.gov/sunsafety/uv-index-scale-0")
+          .attr("target", "_blank");
 
         // change color depending on uv value
         if (data.value < 3) {
